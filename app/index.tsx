00da88186b26acc76ffdac568e12477bff21547d
ananditasharmaa <221210018@nitdelhi.ac.in>
2025-03-14 19:54:31 +0530

    added tabs path

--- app/index.tsx
+++ app/index.tsx
@@ -51,7 +51,7 @@ export default function GetStarted() {
         </Text>
 
         {/* Navigation Button */}
-        <TouchableOpacity style={styles.button} onPress={() => router.replace("/")}>
+        <TouchableOpacity style={styles.button} onPress={() => router.replace("/(tabs)/events")}>
           <Text style={styles.buttonText}>Get Started</Text>
         </TouchableOpacity>
       </View>
