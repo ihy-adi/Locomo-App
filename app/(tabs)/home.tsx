7f71d5a08a03329a0dc7fc0932e04f372a9ba65c
ananditasharmaa <221210018@nitdelhi.ac.in>
2025-03-14 20:22:52 +0530

    packages updated

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -5,7 +5,7 @@ import { Link } from "expo-router";
 export default function RootLayout() {
   return (
     <View className = "flex-1 justify-center items-center">
-      <Text className = "text-5xl text-primary text-primary font-bold">Locomo</Text>
+      <Text className = "text-5xl text-primary font-bold">Locomo</Text>
 
       <Link href="/profile" style = {{color: "red"}}>
         Go to Profile
