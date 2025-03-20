74cb676c9bbed8ab63581b2e4afa0fb5127445be
paprika1212 <aadrika.python@gmail.com>
2025-03-20 10:08:00 +0530

    firebase integration

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,4 +1,16 @@
-import AuthScreen from "@/components/Authscreen";
-export default function Login() {
-  return <AuthScreen type="login" />;
-}
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+const auth = getAuth();
+const email = "user@example.com"; // Replace with actual email input
+const password = "userpassword"; // Replace with actual password input
+
+signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
--- app/(auth)/signup.tsx
+++ app/(auth)/signup.tsx
@@ -1,4 +1,17 @@
-import AuthScreen from "@/components/Authscreen";
-export default function Signup() {
-  return <AuthScreen type="signup" />;
-}
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const auth = getAuth();
+const email = "user@example.com"; // Replace with actual email input
+const password = "userPassword"; // Replace with actual password input
+
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed up 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
