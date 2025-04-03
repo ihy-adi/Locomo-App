e7ed732c5a1b4b222934f8f7bdf7500f30904ae2
Adi <negiaditya2004@gmail.com>
2025-04-03 15:42:32 +0530

    Updated map screen, made search bar and added margins

--- app/(tabs)/maps.tsx
+++ app/(tabs)/maps.tsx
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from "react";
 import { View, StyleSheet } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import * as Location from "expo-location";
+import SearchInput from "../../components/SearchInput";
 
-const MapScreen: React.FC = () => {
+const MapsScreen: React.FC = () => {
   const [location, setLocation] = useState<Location.LocationObject | null>(null);
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
     (async () => {
@@ -20,26 +22,38 @@ const MapScreen: React.FC = () => {
 
   return (
     <View style={styles.container}>
-      <MapView
-        style={styles.map}
-        initialRegion={{
-          latitude: location?.coords.latitude || 28.6139, // Default to New Delhi
-          longitude: location?.coords.longitude || 77.2090,
-          latitudeDelta: 0.1,
-          longitudeDelta: 0.1,
-        }}
-        showsUserLocation={true} // Show blue dot for user location
-      >
-        {location && (
-          <Marker
-            coordinate={{
-              latitude: location.coords.latitude,
-              longitude: location.coords.longitude,
-            }}
-            title="Your Location"
-          />
-        )}
-      </MapView>
+      {/* Search Bar */}
+      <View style={styles.searchContainer}>
+        <SearchInput
+          value={searchText}
+          placeholder="Search location..."
+          handleChangeText={setSearchText}
+        />
+      </View>
+      
+      {/* Map Section */}
+      <View style={styles.mapContainer}>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: location?.coords.latitude || 28.6139,
+            longitude: location?.coords.longitude || 77.2090,
+            latitudeDelta: 0.1,
+            longitudeDelta: 0.1,
+          }}
+          showsUserLocation={true}
+        >
+          {location && (
+            <Marker
+              coordinate={{
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+              }}
+              title="Your Location"
+            />
+          )}
+        </MapView>
+      </View>
     </View>
   );
 };
@@ -47,6 +61,23 @@ const MapScreen: React.FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: "center",
+    paddingTop: 20,
+    paddingBottom: 20,
+  },
+  searchContainer: {
+    width: "90%",
+    marginBottom: 10,
+    marginTop: 15
+  },
+  mapContainer: {
+    width: "90%",
+    height: "78%",
+    backgroundColor: "#ddd",
+    borderRadius: 10,
+    overflow: "hidden",
+    marginTop: 10,
+    marginBottom: 10
   },
   map: {
     width: "100%",
@@ -54,4 +85,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default MapScreen;
+export default MapsScreen;
